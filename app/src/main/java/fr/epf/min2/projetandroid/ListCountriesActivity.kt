a7eaf9e2f650c7eb0e908e879fa7d2fe6eba8adc
ewarskotte <elea.warskotte@epfedu.fr>
2024-05-19 18:13:05 +0200

    First Commit

---
+++ app/src/main/java/fr/epf/min2/projetandroid/ListCountriesActivity.kt
@@ -0,0 +1,20 @@
+package fr.epf.min2.projetandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fr.epf.min2.projetandroid.model.Country
+
+class ListCountriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_countries)
+
+        var recyclerView = findViewById<RecyclerView>(R.id.list_countries_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val countries = Country.generateCountryList()
+
+    }
+}
---
+++ app/src/main/java/fr/epf/min2/projetandroid/model/Country.kt
@@ -0,0 +1,44 @@
+package fr.epf.min2.projetandroid.model
+
+data class Country(
+    val officialName: String,
+    val frenchName: String,
+    val capital: String,
+    val continent: String,
+    val flag: String
+){
+    companion object {
+        fun generateCountryList(): ArrayList<Country> {
+            val countries = ArrayList<Country>()
+            countries.add(
+                Country(
+                    "France",
+                    "France",
+                    "Paris",
+                    "Europe",
+                    "https://flagcdn.com/w320/fr.png"
+                )
+            )
+            countries.add(
+                Country(
+                    "Deutschland",
+                    "Allemagne",
+                    "Berlin",
+                    "Europe",
+                    "https://flagcdn.com/w320/de.png"
+                )
+            )
+            countries.add(
+                Country(
+                    "México",
+                    "Méxique",
+                    "Mexico City",
+                    "North America",
+                    "https://flagcdn.com/w320/mx.png"
+                )
+            )
+            return countries
+        }
+    }
+}
+
